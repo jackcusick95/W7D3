bd017d475f0fa1ebcb22e0784c0d5720113f2044
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-07 15:01:12 -0500

    Begin features test building

---
+++ goal_app/spec/features/auth_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature 'the signup process' do
+  scenario 'has a new user page' do
+    visit new_user_url
+    expect(page).to have_content "Sign up user here"
+  end
+
+  feature 'signing up a user' do
+    before(:each) do 
+        visit new_user_url
+        fill_in 'Username', :with => 'a_username'
+        fill_in 'Password', :with => 'password123'
+        click_on "Create user"
+    end
+    scenario 'shows username on the homepage after signup' do 
+        expect(page).to have_content 'a_username'
+    end
+
+  end
+end
+
+feature 'logging in' do
+  scenario 'shows username on the homepage after login' do
+    visit users_url
+  end
+
+end
+
+feature 'logging out' do
+  scenario 'begins with a logged out state'
+
+  scenario 'doesn\'t show username on the homepage after logout'
+
+end
