ccc590ce51c61df9a6b6407076c722e3b01ae92a
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-07 14:00:28 -0500

    Create specs for User model

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -1,5 +1,42 @@
 
 class User < ApplicationRecord
 
+    validates :username, :password_digest, :session_token, presence: true
+
+    attr_reader :password 
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user 
+        else
+            return nil
+        end
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom.urlsafe_base64(16)
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token 
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
 
 end 
